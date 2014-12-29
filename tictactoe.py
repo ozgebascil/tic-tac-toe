62c68bbf71bf0e494b4ee7ee23690b17a7de8e50
Ozge Bascil <ozge14@gmail.com>
2014-12-29 16:21:23 +0000

    Created input function

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,37 @@
+X = 'x'
+O = 'o'
+E = 'e'
+
+
+def get_input(symbol):
+    coord = raw_input("Please enter coordinates for %s in the following format 1 2  : " % symbol)
+    coord = coord.split()
+
+    if len(coord) != 2:
+        print "INVALID INPUT!"
+        return get_input(symbol)
+
+    x, y = coord
+
+    try:
+        x = int(x)
+        y = int(y)
+    except ValueError:
+        print "INVALID INPUT!"
+        return get_input(symbol)
+
+    if not(x in [0, 1, 2] and y in [0, 1, 2]):
+        print "INVALID INPUT!"
+        return get_input(symbol)
+
+    return x, y
+
+
+
 def run_game():
     print "GAME"
 
+
 if __name__ == "__main__":
-    run_game()
+    #run_game()
+    print get_input(X)
