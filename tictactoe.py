52a1ca24e60cf520aeb4d370905050c99b781793
Ozge Bascil <ozge14@gmail.com>
2014-12-29 21:56:07 +0000

    Created check board function

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,10 @@
 X = 'X'
 O = 'O'
 E = '_'
+X_WIN = 0
+O_WIN = 1
+DRAW = 2
+CONTINUE = 3
 
 
 def get_input(symbol):
@@ -41,6 +45,21 @@ def print_board(board):
         print " ".join(row)
 
 
+def check_board(board):
+    diag1 = [board[0][0], board[1][1], board[2][2]]
+    diag2 = [board[0][2], board[1][1], board[2][0]]
+    columns = []
+    for i in range(3):
+        column = [row[i] for row in board]
+        columns.append(column)
+
+    lst = board + columns + [diag1, diag2]
+
+    if [X, X, X] in lst:
+        return X_WIN
+    elif [O, O, O] in lst:
+        return O_WIN
+
 
 def run_game():
     print "GAME"
@@ -48,5 +67,6 @@ def run_game():
 
 if __name__ == "__main__":
     #run_game()
-    print update_board(X, 0, 0, [[X, E, O], [E, E, E], [O, E, X]])
+    #print update_board(X, 0, 0, [[X, E, O], [E, E, E], [O, E, X]])
     #print_board([[X, E, O], [E, E, E], [O, E, X]])
+    print check_board([[X, E, O], [X, E, O], [E, E, O]])
